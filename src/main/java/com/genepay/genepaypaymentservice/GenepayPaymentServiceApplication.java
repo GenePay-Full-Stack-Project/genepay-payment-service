3381b947ae6f028b007511fd93ff40f7b8ccda3b
Udara Wickramarathne <bimsaraudara25@gmail.com>
2026-01-09 19:35:01 +0530

    initial commit

---
+++ src/main/java/com/genepay/genepaypaymentservice/GenepayPaymentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.genepay.genepaypaymentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenepayPaymentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenepayPaymentServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/genepay/genepaypaymentservice/GenepayPaymentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.genepay.genepaypaymentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenepayPaymentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
