570e85e6232a843362fd2cfff7dddc7f47a7c89f
Janindu Pramod <janindupramod@gmail.com>
2026-02-01 23:34:47 +0530

    feat(card): add methods to find default cards by user and merchant

--- src/main/java/com/genepay/genepaypaymentservice/controller/MerchantController.java
+++ src/main/java/com/genepay/genepaypaymentservice/controller/MerchantController.java
@@ -11,6 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import io.swagger.v3.oas.annotations.Parameter;
 
 @Slf4j
 @RestController
