88bbfa26239dc8f5b29b4ec131381cb38f4201c4
Udara Wickramarathne <bimsaraudara25@gmail.com>
2026-02-14 11:33:01 +0530

    feat: add user profile update functionality

---
+++ src/main/java/com/genepay/genepaypaymentservice/dto/UpdateUserRequest.java
@@ -0,0 +1,24 @@
+package com.genepay.genepaypaymentservice.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UpdateUserRequest {
+
+    @Email(message = "Invalid email format")
+    private String email;
+
+    @Size(min = 8, message = "Password must be at least 8 characters")
+    private String password;
+
+    private String fullName;
+    private String phoneNumber;
+}
